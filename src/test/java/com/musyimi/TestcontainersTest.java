49d04a522ece505bc69b2d07c5b2d269deb1a9ec
musyimi <musyimi13@gmail.com>
2023-07-17 13:34:04 +0300

    created new class abstractTestContainers for logic

--- src/test/java/com/musyimi/TestcontainersTest.java
+++ src/test/java/com/musyimi/TestcontainersTest.java
@@ -10,46 +10,14 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Testcontainers
-public class TestcontainersTest {
 
-    @Container
-    private static final PostgreSQLContainer<?> postgreSQLContainer =
-            new PostgreSQLContainer<>("postgres:latest")
-                    .withDatabaseName("musyimi-dao-unit-test")
-                    .withUsername("musyimi")
-                    .withPassword("password");
+public class TestcontainersTest extends AbstractTestcontainersUnitTest {
 
-    @DynamicPropertySource
-    private static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
-        registry.add(
-                "spring.datasource.url",
-                postgreSQLContainer::getJdbcUrl
-        );
-        registry.add(
-                "spring.datasource.username",
-                postgreSQLContainer::getUsername
-        );
-        registry.add(
-                "spring.datasource.password",
-                postgreSQLContainer::getPassword
-        );
-    }
 
     @Test
     void canStartPostgresDB() {
         assertThat(postgreSQLContainer.isRunning()).isTrue();
         assertThat(postgreSQLContainer.isCreated()).isTrue();
-        //assertThat(postgreSQLContainer.isHealthy()).isTrue();
     }
 
-    @Test
-    void canApplyDbMigrationsWithFlyway() {
-        Flyway flyway = Flyway.configure().dataSource(
-                postgreSQLContainer.getJdbcUrl(),
-                postgreSQLContainer.getUsername(),
-                postgreSQLContainer.getPassword()
-        ).load();
-        flyway.migrate();
-    }
 }
