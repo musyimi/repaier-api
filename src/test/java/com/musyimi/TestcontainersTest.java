34f6af0b319bda061496f0bb8e2042bff424e569
musyimi <musyimi13@gmail.com>
2023-07-17 13:05:31 +0300

    added testContainerclass and two test methods

---
+++ src/test/java/com/musyimi/TestcontainersTest.java
@@ -0,0 +1,37 @@
+package com.musyimi;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+public class TestcontainersTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("musyimi-dao-unit-test")
+                    .withUsername("musyimi")
+                    .withPassword("password");
+
+    @Test
+    void canStartPostgresDB() {
+        assertThat(postgreSQLContainer.isRunning()).isTrue();
+        assertThat(postgreSQLContainer.isCreated()).isTrue();
+        //assertThat(postgreSQLContainer.isHealthy()).isTrue();
+    }
+
+    @Test
+    void canApplyDbMigrationsWithFlyway() {
+        Flyway flyway = Flyway.configure().dataSource(
+                postgreSQLContainer.getJdbcUrl(),
+                postgreSQLContainer.getUsername(),
+                postgreSQLContainer.getPassword()
+        ).load();
+        flyway.migrate();
+    }
+}
