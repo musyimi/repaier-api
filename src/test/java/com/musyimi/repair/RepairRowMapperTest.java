38028d106ee4c51dbb576338b9dd475c69f66525
musyimi <musyimi13@gmail.com>
2023-07-18 14:10:39 +0300

    tested rowMapper class but not working

---
+++ src/test/java/com/musyimi/repair/RepairRowMapperTest.java
@@ -0,0 +1,36 @@
+package com.musyimi.repair;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class RepairRowMapperTest {
+
+    @Test
+    void mapRow() throws SQLException {
+
+        RepairRowMapper repairRowMapper = new RepairRowMapper();
+        ResultSet resultSet = mock(ResultSet.class);
+
+        when(resultSet.getInt("id")).thenReturn(1);
+        when(resultSet.getInt("phone_number")).thenReturn(900000000);
+        when(resultSet.getString("issue")).thenReturn("not charging");
+        when(resultSet.getString("brand")).thenReturn("Nokia");
+        when(resultSet.getString("name")).thenReturn("Kaku");
+        when(resultSet.getString("title")).thenReturn("Nokia 3310");
+
+        Repair actual = repairRowMapper.mapRow(resultSet, 1);
+
+        Repair expected = new Repair(
+                1,"Kaku", "Nokia 3310", "Nokia", "not charging", 900000000
+        );
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
